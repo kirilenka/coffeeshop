ac5019ede5c6f9ed2bb1a7b4e9bb98db9aa2c7c5
Agil Kerimov <ahkerimov@gmail.com>
2020-02-12 14:24:31 +0400

    Mobile

---
+++ assets/js/index.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', e => {
+    document.getElementById('hamburger').classList.toggle('open');
+    document.getElementById('nav').classList.toggle('open');
+});
